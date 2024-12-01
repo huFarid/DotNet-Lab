8fffd365bc62e10556ebcbddf91ba85d25bede0e
Hossein <h.younesian.farid@gmail.com>
2024-12-01 12:49:06 +0100

    WebAPI practice

---
+++ 3_WebAPI_ASP_DotNet_Core/LocationInfo/CitiesDataStore.cs
@@ -0,0 +1,19 @@
+﻿using LocationInfo.Models;
+
+namespace LocationInfo
+{
+    public class CitiesDataStore
+    {
+        public List<CityDTO> Cities { get; set; }
+        public static CitiesDataStore citiesDataStore { get; } = new CitiesDataStore();
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDTO>()
+            {
+                new CityDTO() { Id = 1, Name = "Tehran", Description = "Capital" },
+                new CityDTO() { Id = 2, Name = "Hamedan", Description = "Historica" },
+                new CityDTO() { Id = 3, Name = "Esfahan", Description = "Industrial" }
+            };
+        }
+    }
+}
---
+++ 3_WebAPI_ASP_DotNet_Core/LocationInfo/Controllers/CitiesController.cs
@@ -0,0 +1,43 @@
+﻿using LocationInfo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LocationInfo.Controllers
+{
+
+    [ApiController]
+    [Route("/api/Cities")] 
+    
+    public class CitiesController:ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CityDTO>> GetCities()
+        {
+
+
+            return Ok(CitiesDataStore.citiesDataStore.Cities);
+        }
+
+
+
+
+        [HttpGet("/{Id}")]
+        public ActionResult GetCity(int Id)
+        {
+            var city = CitiesDataStore.citiesDataStore.Cities.FirstOrDefault(x => x.Id == Id);
+
+            if (city != null) 
+            {
+                return Ok(city);
+            }
+            return NotFound();
+
+            
+        }
+
+
+
+
+    }
+}
