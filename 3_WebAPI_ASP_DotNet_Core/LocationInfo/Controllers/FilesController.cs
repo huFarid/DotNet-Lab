20109a0541439de65cdaaefb0cd31cd489fea027
Hossein <h.younesian.farid@gmail.com>
2024-12-04 23:45:14 +0100

    Updating City information with HttpPut

---
+++ 3_WebAPI_ASP_DotNet_Core/LocationInfo/Controllers/FilesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace LocationInfo.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+        public FilesController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+        {
+            _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider;
+            
+        }
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId)
+        {
+            var pathToFile = "image1.PNG";
+            if (!System.IO.File.Exists(pathToFile))
+            {
+                return NotFound();
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+
+
+            
+            if (! _fileExtensionContentTypeProvider.TryGetContentType(pathToFile , out var contentType))
+            {
+                contentType = "application/octec-stream";
+            }
+
+
+            return File(bytes, contentType, Path.GetFileName(pathToFile));
+            
+
+        }
+
+        
+    }
+}
---
+++ 3_WebAPI_ASP_DotNet_Core/LocationInfo/Models/TouristicLocationsForCreationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace LocationInfo.Models
+{
+    public class TouristicLocationsForCreationDto
+    {
+
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } 
+    }
+}
